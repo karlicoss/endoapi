3057e0df43d26df2f0fa3a86185b8439b20226a8
Piotr Podusowski <podusowski@gmail.com>
2016-08-21 04:34:36 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Endoapi',
+      version='1.0',
+      description='Unofficial API for Endomondo based on https://github.com/yannickcarer/endomondo-export',
+      author='Piotr',
+      author_email='podusowski@gmail.com',
+      url='https://github.com/podusowski/endoapi',
+      packages=['endoapi'],
+     )
